8a5127c8015ad2b1362d170762309e245b9912f6
Amish Tufail <amish.tufail2002@gmail.com>
2023-04-20 01:19:06 +0500

    Initial Commit

---
+++ Drinkie/DrinkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkieApp.swift
+//  Drinkie
+//
+//  Created by Amish Tufail on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrinkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
